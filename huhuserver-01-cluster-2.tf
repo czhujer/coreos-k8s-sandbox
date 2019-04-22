831bce715f492b257aceb65e14ec9b17ea59996b
Patrik Majer <patrik.majer@atlas.cz>
2019-04-22 14:14:24 +0200

    fix provider and disk params

--- huhuserver-01-cluster-2.tf
+++ huhuserver-01-cluster-2.tf
@@ -1,18 +1,23 @@
 # docs
 #https://github.com/dmacvicar/terraform-provider-libvirt
 #https://computingforgeeks.com/how-to-provision-vms-on-kvm-with-terraform/
+# for coreos
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/examples/coreos/main.tf
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/examples/coreos/ignition.tf
 
+# debug virsh -c qemu+ssh://root@192.168.10.241/system list
 provider "libvirt" {
-  alias = "huhuserver-01"
+#  alias = "huhuserver-01"
   uri   = "qemu+ssh://root@192.168.10.241/system"
 }
 
-resource "libvirt_volume" "coreos-c2-s1" {
-  name = "coreos-c1-s1.qcow2"
+resource "libvirt_volume" "coreos-c2-s1-disk0" {
+  name = "coreos-c2-s1-disk0.qcow2"
   pool = "vm_storage"
   #source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
   #source = "./CentOS-7-x86_64-GenericCloud.qcow2"
   format = "qcow2"
+  size    = "20"
 }
 
 # Define KVM domain to create
